96fbfedaf991a74d080f715870524a258c8578a1
bvolcan <brunovolcan@gmail.com>
2023-04-27 15:18:55 -0300

    wip: working

--- nRainhasV2.c
+++ nRainhasV2.c
@@ -2,92 +2,64 @@
 #include <stdlib.h>
 #include <omp.h>
 
-int n;
-int solucao[100];
-int total_solucoes = 0;
-int solucoes_unicas[92][8];
-int indice_unicas = 0;
+int count = 0;
 
-int valido(int linha, int coluna) {
-    int i;
-    for (i = 0; i < linha; i++) {
-        if (solucao[i] == coluna || 
-            solucao[i] - coluna == i - linha ||
-            coluna - solucao[i] == i - linha) {
+int is_valid(int *board, int row, int col) {
+    int i, j;
+    for (i = 0; i < row; i++) {
+        if (board[i] == col || abs(i - row) == abs(board[i] - col)) {
             return 0;
         }
     }
     return 1;
 }
 
-int solucao_repetida(int nova_solucao[]) {
-    int i, j;
-    for (i = 0; i < indice_unicas; i++) {
-        for (j = 0; j < n; j++) {
-            if (nova_solucao[j] != solucoes_unicas[i][j]) {
-                break;
-            }
-        }
-        if (j == n) {
-            return 1;
-        }
-    }
-    return 0;
-}
-
-void busca(int linha) {
+void n_queens(int *board, int n, int row) {
     int i;
-    if (linha == n) {
-        // encontrou uma solução
-        int j;
-        total_solucoes++;
-        printf("Solucao número %d encontrada:\n", total_solucoes);
+    if (row == n) {
+        #pragma omp atomic
+        count++;
+    } else {
         for (i = 0; i < n; i++) {
-            for (j = 0; j < solucao[i]; j++) {
-                printf(". ");
+            if (is_valid(board, row, i)) {
+                board[row] = i;
+                n_queens(board, n, row+1);
             }
-            printf("R ");
-            for (j = solucao[i] + 1; j < n; j++) {
-                printf(". ");
-            }
-            printf("\n");
-        }
-        printf("\n");
-        // adiciona solução única
-        int nova_solucao[8];
-        for (i = 0; i < n; i++) {
-            nova_solucao[i] = solucao[i];
-        }
-        if (!solucao_repetida(nova_solucao)) {
-            for (i = 0; i < n; i++) {
-                solucoes_unicas[indice_unicas][i] = nova_solucao[i];
-            }
-            indice_unicas++;
-        }
-        return;
-    }
-    #pragma omp parallel for
-    for (i = 0; i < n; i++) {
-        if (valido(linha, i)) {
-            solucao[linha] = i;
-            busca(linha + 1);
         }
     }
 }
 
 int main() {
-    int i, t;
-    double start, end;
-    printf("Digite o número de rainhas: ");
+    int n, num_threads;
+    double start_time, end_time;
+    printf("Digite o valor de N: ");
     scanf("%d", &n);
     printf("Digite o número de threads: ");
-    scanf("%d", &t);
-    omp_set_num_threads(t);
-    start = omp_get_wtime();
-    busca(0);
-    end = omp_get_wtime();
-    printf("Tempo de execucao: %.2f segundos\n", end - start);
-    printf("Total de soluções encontradas: %d\n", total_solucoes);
-    printf("Total de soluções únicas encontradas: %d\n", indice_unicas);
+    scanf("%d", &num_threads);
+    int *board = (int*) malloc(sizeof(int) * n);
+    int i;
+    for (i = 0; i < n; i++) {
+        board[i] = -1;
+    }
+    omp_set_num_threads(num_threads);
+    start_time = omp_get_wtime();
+    #pragma omp parallel
+    {
+        int *board_private = (int*) malloc(sizeof(int) * n);
+        int j;
+        for (j = 0; j < n; j++) {
+            board_private[j] = -1;
+        }
+        #pragma omp for
+        for (i = 0; i < n; i++) {
+            board_private[0] = i;
+            n_queens(board_private, n, 1);
+        }
+        free(board_private);
+    }
+    end_time = omp_get_wtime();
+    printf("Número total de soluções possíveis: %d\n", count);
+    printf("Tempo de execução: %f segundos\n", end_time - start_time);
+    free(board);
     return 0;
 }
